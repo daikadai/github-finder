651256e2f4c0e0292a55bedcd86a63044eae3fa6
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-03-05 11:30:28 +0700

    lesson-1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
+    const name = "John Doe";
+    const loading = false;
+    const showName = true;
     return (
       <div className="App">
-
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+            <h1>Hello{showName ? name : null}</h1>
+          )}
       </div>
     );
   }
