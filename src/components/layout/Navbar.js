b754973d790e928341732150a89031eca8550db7
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-03-06 16:55:57 +0700

    Single User Component & Data

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
 
 const Navbar = ({ icon, title }) => {
 
@@ -10,6 +10,10 @@ const Navbar = ({ icon, title }) => {
         <i className={icon}></i>
         {title}
       </h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </nav>
   )
 }
