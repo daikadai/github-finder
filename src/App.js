5d23a0dd60c365bdfb28e94e885530500fffe151
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-03-06 15:54:22 +0700

    Alert State & Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
   // async componentDidMount() {
   //   this.setState({ loading: true });
@@ -26,6 +28,13 @@ class App extends Component {
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
     const { users, loading } = this.state;
 
@@ -33,7 +42,8 @@ class App extends Component {
       <div className="App">
         <Navbar icon='fab fa-github' />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
+          <Alert alert={this.state.alert} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} setAlert={this.setAlert} />
           <Users loading={loading} users={users} />
         </div>
       </div>
